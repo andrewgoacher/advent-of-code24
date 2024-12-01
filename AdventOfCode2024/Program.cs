b848d20c912dee7d67e680df240feecd0ececb0a
Goach <andrewgoacher@outlook.com>
2024-12-01 14:38:48 +0000

    Solve part 1

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AdventOfCode2024;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Day 1");
+var day1Input = await File.ReadAllLinesAsync("inputs/Day1.txt");
+Console.WriteLine($"Part 1: {Day1Solver.Part1(day1Input)}");
