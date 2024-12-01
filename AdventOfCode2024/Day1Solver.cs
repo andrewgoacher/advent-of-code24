a899fbc12829309aae10037089d9f8e4110cdaee
Goach <andrewgoacher@outlook.com>
2024-12-01 14:35:14 +0000

    solve day 1 part 1 example code

--- AdventOfCode2024/Day1Solver.cs
+++ AdventOfCode2024/Day1Solver.cs
@@ -4,6 +4,46 @@ public static class Day1Solver
 {
     public static int Part1(string[] input)
     {
-        return 0;
+        var lines = Collect(input);
+        var lhs = lines[0].Order();
+        var rhs = lines[1].Order();
+
+        return lhs.Zip(rhs, AbsoluteDistance)
+            .Sum();
+
+        static int AbsoluteDistance(int lhs, int rhs) =>
+            Math.Abs(lhs - rhs);
+    }
+
+    private static int[][] Collect(string[] input)
+    {
+        var items = new int[2][];
+        items[0] = new int[input.Length];
+        items[1] = new int[input.Length];
+
+        var lines = input
+            .Select(Split)
+            .Select(Transform)
+            .ToArray();
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var (item1, item2) = lines[i];
+            items[0][i] = item1;
+            items[1][i] = item2;
+        }
+
+        return items;
+
+        static (int, int) Transform(string[] line)
+        {
+            var item1 = int.Parse(line[0]);
+            var item2 = int.Parse(line[1]);
+
+            return (item1, item2);
+        }
+
+        static string[] Split(string line) =>
+            line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
     }
 }
