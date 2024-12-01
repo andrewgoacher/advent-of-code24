ff762128bb550021532dc5b14bf7be8478b7cee7
Goach <andrewgoacher@outlook.com>
2024-12-01 14:26:46 +0000

    Add basic solver and test for part 1 (day 1)

---
+++ AdventOfCode2024/Day1Solver.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode2024;
+
+public static class Day1Solver
+{
+    public static int Part1(string[] input)
+    {
+        return 0;
+    }
+}
---
+++ Tests/Day1Tests.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2024;
+
+namespace Tests;
+
+public class Day1Tests
+{
+    [Fact]
+    public void Day1_WithExampleInputs_GivesExpectedResult()
+    {
+        string[] exampleInput = [
+            "3   4",
+            "4   3",
+            "2   5",
+            "1   3",
+            "3   9",
+            "3   3"
+        ];
+
+        var result = Day1Solver.Part1(exampleInput);
+
+        Assert.Equal(11, result);
+    }
+}
