88ea817c204bd38b29b7c59d3d92f66ff043951b
Andrew Goacher <andrewgoacher@outlook.com>
2024-12-05 16:56:03 +0000

    Complete part 1

--- AdventOfCode2024/Solvers/Day5.cs
+++ AdventOfCode2024/Solvers/Day5.cs
@@ -2,6 +2,8 @@
 
 public class Day5 : Solver
 {
+    private readonly Dictionary<int, List<int>> _replacements = new();
+
     private int[][] _rows = null!;
     public Day5(string[] input) : base(input)
     {
@@ -11,23 +13,69 @@ public class Day5 : Solver
 
     protected override void OnPrepare(string[] inputs)
     {
-        _rows = new int[inputs.Length][];
+        List<int[]> rows = new();
+
         for (var i = 0; i < inputs.Length; ++i)
         {
-            _rows[i] = inputs[i].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+            var line = inputs[i];
+            if (line.Contains('|'))
+            {
+                var values = line.Split(new [] { '|'}, StringSplitOptions.RemoveEmptyEntries);
+                var lhs = int.Parse(values[0]);
+                var rhs = int.Parse(values[1]);
+                if (_replacements.TryGetValue(rhs, out var replacements))
+                {
+                    replacements.Add(lhs);
+                }
+                else
+                {
+                    _replacements.Add(rhs, new List<int> { lhs });
+                }
+
+                continue;
+            }
+
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                continue;
+            }
+            var row = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
+            rows.Add(row);
         }
+
+        _rows = rows.ToArray();
     }
 
     protected override int Part1()
     {
-        return _rows.Select(GetMidPoint).Sum();
+        return _rows
+            .Where(IsInCorrectOrder)
+            .Select(GetMidPoint).Sum();
     }
 
     protected override int Part2()
     {
         return 0;
+    }
+
+    private bool IsInCorrectOrder(int[] row)
+    {
+        for (var i = 0; i < row.Length; ++i)
+        {
+            var item = row[i];
+            if (_replacements.TryGetValue(item, out var replacements))
+            {
+                var slice = row[i..];
+                if (replacements.Exists(x => slice.Contains(x)))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     private static int GetMidPoint(int[] row)
--- Tests/Day5Tests.cs
+++ Tests/Day5Tests.cs
@@ -4,6 +4,38 @@ namespace Tests;
 
 public class Day5Tests
 {
+    private readonly string[] _exampleInput = new[]
+    {
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+        "",
+        "75,47,61,53,29",
+        "97,61,53,29,13",
+        "75,29,13",
+        "75,97,47,61,53",
+        "61,13,29",
+        "97,13,75,29,47",
+    };
+
     [Fact]
     public void NoMappings_SumsMiddleNumbers()
     {
@@ -21,5 +53,13 @@ public class Day5Tests
         var expected = 61 + 53 + 29 + 47 + 13 + 75;
 
         Assert.Equal(expected, part1);
+    }
+
+    [Fact]
+    public void Day5_Part1_Example()
+    {
+        var (part1, _) = new Day5(_exampleInput).Solve();
+
+        Assert.Equal(143, part1);
     }
 }
