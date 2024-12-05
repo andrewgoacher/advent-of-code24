d735971f51ebee29a18b0e4515bd5a3fc6ac84ee
Andrew Goacher <andrewgoacher@outlook.com>
2024-12-05 16:31:10 +0000

    Correctly sum midpoint or a row

--- AdventOfCode2024/Solvers/Day5.cs
+++ AdventOfCode2024/Solvers/Day5.cs
@@ -2,19 +2,42 @@
 
 public class Day5 : Solver
 {
+    private int[][] _rows = null!;
     public Day5(string[] input) : base(input)
     {
     }
 
     public static Day5 FromFile() => new Day5(File.ReadAllLines("inputs/Day5.txt"));
 
+    protected override void OnPrepare(string[] inputs)
+    {
+        _rows = new int[inputs.Length][];
+        for (var i = 0; i < inputs.Length; ++i)
+        {
+            _rows[i] = inputs[i].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+
     protected override int Part1()
     {
-        return 0;
+        return _rows.Select(GetMidPoint).Sum();
     }
 
     protected override int Part2()
     {
         return 0;
+    }
+
+    private static int GetMidPoint(int[] row)
+    {
+        var len = row.Length;
+        if (len % 2 == 1)
+        {
+            len += 1;
+        }
+
+        return row[(len / 2)-1];
     }
 }
---
+++ Tests/Day5Tests.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode2024.Solvers;
+
+namespace Tests;
+
+public class Day5Tests
+{
+    [Fact]
+    public void NoMappings_SumsMiddleNumbers()
+    {
+        var inputs = new string[]
+        {
+            "75,47,61,53,29",
+            "97,61,53,29,13",
+            "75,29,13",
+            "75,97,47,61,53",
+            "61,13,29",
+            "97,13,75,29,47"
+        };
+
+        var (part1, _) = new Day5(inputs).Solve();
+        var expected = 61 + 53 + 29 + 47 + 13 + 75;
+
+        Assert.Equal(expected, part1);
+    }
+}
