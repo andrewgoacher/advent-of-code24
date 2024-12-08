09fc4261eb4580bd292c5450737119c2dca2bcef
Goach <andrewgoacher@outlook.com>
2024-12-08 12:38:20 +0000

    Day 8 initial commit

---
+++ AdventOfCode2024/Solvers/Day8.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2024.Solvers;
+
+public class Day8 : Solver
+{
+    public Day8(string[] input) : base(input)
+    {
+    }
+
+    public static Day8 FromFile() => new Day8(File.ReadAllLines("inputs/Day8.txt"));
+
+    protected override long Part1() => 0;
+
+    protected override long Part2() => 0;
+}
